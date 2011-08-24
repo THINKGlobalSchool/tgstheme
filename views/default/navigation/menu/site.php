568603707b566acfc4eae4429f67cd048a335761
Jeff Tilson <jrtilson@gmail.com>
2011-08-24 11:58:51 -0400

    'Browse' menu lives next to search (hacky)

--- views/default/navigation/menu/site.php
+++ views/default/navigation/menu/site.php
@@ -10,7 +10,8 @@ echo '<ul class="elgg-menu elgg-menu-site elgg-menu-site-default clearfix">';
 foreach ($vars['menu']['default'] as $menu_item) {
 	echo elgg_view('navigation/menu/elements/item', array('item' => $menu_item));
 }
-
+echo '</ul>';
+echo '<ul class="elgg-menu elgg-menu-more-right elgg-menu-site elgg-menu-site-default clearfix">';
 if (isset($vars['menu']['more'])) {
 	echo '<li class="elgg-more">';
 
