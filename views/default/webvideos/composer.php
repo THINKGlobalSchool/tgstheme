c92964643d23e29e5d9f5875d3381a2bb57d3884
Jeff Tilson <jrtilson@gmail.com>
2011-09-29 16:24:00 -0400

    Added composer tab for web videos. Added video icon

---
+++ views/default/webvideos/composer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Jeff Tilson
+ */
+
+// General form vars
+$form_vars = array(
+	'id' => 'webvideos-save-form', 
+	'name' => 'webvideos-save-form'
+);
+
+echo elgg_view_form('webvideos/save', $form_vars);
