3155ebb4f89fd15f3eb634a1bc83dd507877e006
Jeff Tilson <jrtilson@gmail.com>
2013-07-10 15:38:27 -0400

    Removed new content_one_column view in favor of overriding the one_sidebar view and conditionally displaying a sidebar

---
+++ views/default/page/layouts/one_column.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Elgg one-column layout
+ *
+ * OVERRIDE:
+ * - Added extras menu
+ *
+ * @package Elgg
+ * @subpackage Core
+ *
+ * @uses $vars['content'] Content string
+ * @uses $vars['class']   Additional class to apply to layout
+ */
+
+$class = 'elgg-layout elgg-layout-one-column clearfix';
+if (isset($vars['class'])) {
+	$class = "$class {$vars['class']}";
+}
+
+// navigation defaults to breadcrumbs
+$nav = elgg_extract('nav', $vars, elgg_view('navigation/breadcrumbs'));
+
+$extras = elgg_view_menu('extras', array(
+	'sort_by' => 'priority',
+	'class' => 'elgg-menu-hz',
+));
+
+?>
+<div class="<?php echo $class; ?>">
+	<div class="elgg-body elgg-main">
+	<?php
+		echo $nav;
+		echo $extras;
+
+		if (isset($vars['title'])) {
+			echo elgg_view_title($vars['title']);
+		}
+
+		echo $vars['content'];
+		
+		// @deprecated 1.8
+		if (isset($vars['area1'])) {
+			echo $vars['area1'];
+		}
+	?>
+	</div>
+</div>
