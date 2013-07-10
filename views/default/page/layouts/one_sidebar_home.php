bc9984af2466ba4235b35950e1d8a9a4b62dcd69
Jeff Tilson <jrtilson@gmail.com>
2013-07-10 11:35:49 -0400

    Moved sidebar to the left

--- views/default/page/layouts/one_sidebar_home.php
+++ views/default/page/layouts/one_sidebar_home.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * TGS Theme 2
- * Custom Layout for main column with one sidebar on the right
+ * Custom Layout for home page with main content on the right and a wide sidebar on the left
  *
  * @package TGSTheme2
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
