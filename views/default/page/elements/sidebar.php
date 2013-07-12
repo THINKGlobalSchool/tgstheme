350a73e599677c2f1e55c1c1a687fa44a7d2d297
Jeff Tilson <jrtilson@gmail.com>
2013-07-12 17:17:50 -0400

    Moved menu back below owner block

--- views/default/page/elements/sidebar.php
+++ views/default/page/elements/sidebar.php
@@ -8,9 +8,9 @@
  * @uses $vars['sidebar'] Optional content that is displayed at the bottom of sidebar
  */
 
-echo elgg_view_menu('page', array('sort_by' => 'name'));
-
 echo elgg_view('page/elements/owner_block', $vars);
+
+echo elgg_view_menu('page', array('sort_by' => 'name'));
 
 // optional 'sidebar' parameter
 if (isset($vars['sidebar'])) {
