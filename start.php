5244d1bae924a00716fbd4b5503907eb28c3a9bd
Jeff Tilson <jrtilson@gmail.com>
2014-03-28 13:13:14 -0400

    Implementing custom jquery ui css

--- start.php
+++ start.php
@@ -108,6 +108,11 @@ function tgstheme_init() {
 	elgg_register_css('chosen.js', $c_css);
 	elgg_load_css('chosen.js');
 
+	// Register jquery ui css
+	$css = elgg_get_simplecache_url('css', 'jquery.ui');
+	elgg_register_simplecache_view('css/jquery.ui');
+	elgg_register_css('elgg.jquery.ui', $css);
+
 	if (elgg_get_context() == 'activity') {
 		elgg_load_js('elgg.activityping');
 	}
